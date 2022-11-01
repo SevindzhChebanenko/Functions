bdd290ea1f29523b742057925095e8e49944760a
Chebanenko Sevindzh <se8a16@mail.ru>
2022-11-01 15:28:23 +0500

    new task

--- app.js
+++ app.js
@@ -28,3 +28,45 @@ function promotion (result) {
 convert(500, eurCurr)
 const res = convert(500, usdCurr)
 promotion(res)
+
+
+
+// Место для первой задачи
+function sayHello(name) {
+    return name
+
+}
+
+sayHello('Антон')
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num +1]
+}
+
+returnNeighboringNumbers(5)
+
+
+// Место для третьей задачи
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
